ae66dc63947b9576fb7896b5087b807479027acd
dcollins <dcollins4096@gmail.com>
2024-12-12 16:00:55 -0500

    fake_powerlaw on its own

--- math/brunt_tools.py
+++ math/brunt_tools.py
@@ -52,43 +52,6 @@ def get_cubes(sim,frame,do_rho_4=False):
 
     return output
 
-def fake_powerlaw(N,alphaT,kmin,kmax,Amplitude=1,phase=False, rando=False):
-    kI = np.fft.fftfreq(N)
-    kx,ky,kz=np.meshgrid(kI,kI,kI)
-    rrr = np.sqrt(kx**2+ky**2+kz**2)
-    Ahat = np.zeros_like(rrr*1j)
-    k = np.unique(np.abs(kx))
-
-    rmin = k[kmin]
-    rmax = k[kmax]
-
-    ok = (rrr>rmin)*(rrr<=rmax)
-    ok_r=ok
-
-    #alphaT is total power. 2 makes a flat power
-    alphaV=alphaT-2 #alphaV is volume avg power.
-    alphaprime=(alphaT-2)/2
-    Ahat[ok]=Amplitude*rrr[ok]**alphaprime
-    print("Alpha prime",alphaprime)
-    #Ahat -= Ahat.min()-4
-    if rando:
-        rando=np.random.random(Ahat.size)
-        rando.shape=Ahat.shape
-        Ahat *= rando
-    Ahat[0,0,0] =100*Ahat.max()
-    #Ahat=np.ones([N,N,N])
-    if phase:
-        phi = (np.random.random(Ahat.size)-0.5)*np.pi*2
-        phi.shape = Ahat.shape
-        print('PHI',phi.min(),phi.max())
-        Ahatmag = np.abs(Ahat)
-        Ahat = Ahatmag*np.cos(phi)+Ahatmag*np.sin(phi)*1j
-        Ahat[0,0,0] = np.abs(Ahat[0,0,0])
-    H = Ahat.shape[0]//2
-    Atotal=Ahat
-    Ahat = Ahat[:,:,:H+1]
-    Q = np.fft.irfftn(Ahat)
-    return Q
 
 
 def plot_set(ftool,outname,axis=0):
